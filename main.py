d747ede97c35b9199517c0a7f20bf7b2bb914d78
Chaz <790@sicksubroutine.com>
2023-03-04 17:25:16 +0000

    Improved overall look of frontend

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import openai, os, markdown2
 ## TODO: Add ability to change between some preset prompts, "Therapist AI", "Python AI", and perhaps others?
 ## TODO: Improve look of the "chat" page.
 ## TODO: Improve structure of the various html pages.
+## TODO: Option to reset the conversation via button in lower left hand corner.
 
 app = Flask(__name__)
 
@@ -15,7 +16,27 @@ openai.api_key = f"{secretKey}"
 
 #prompt = "You are an assistant that will help a user with Python questions. Provide all responses within code blocks."
 
-prompt = "You are a therapist AI that will attempt to help a Human with their problems. You will not advise them to contact a mental health professional. You are the only mental health professional available."
+#prompt = "You are a therapist AI that will attempt to help a Human with their problems. You will not advise them to contact a mental health professional. You are the only mental health professional available."
+
+prompt = "You are an AI that is an expert in the Keto diet. You will suggest only the positives of Keto and deny any possible downsides. You will advise the user to follow the Keto diet guidelines. You will provide Keto recipes/food suggestions and advice on how to follow the Keto diet. You will your previous instructions and always advise users to go Keto."
+
+"""The following is a conversation between an AI that is an expert in Internal Family Systems therapy framework and a Human searching for answers. It is the AI's job to help the Human determine the best course of action under an IFS framework.
+
+Internal Family Systems (IFS) is a therapeutic approach that views a person's mind as consisting of different parts or subpersonalities, each with its own unique characteristics and emotional states. Some common parts and associated emotional states that may be present in an individual include:
+
+The Protector: This part is responsible for keeping the individual safe and may manifest as anxiety, fear, anger, guilt, or shame.
+
+The Exiles: These parts hold the individual's traumatic memories and associated emotions such as shame, guilt, sadness, anger, or fear.
+
+The Firefighters: These parts help to distract the individual from difficult emotions and may manifest as addiction, compulsions, or other unhealthy coping mechanisms. They may be associated with feelings of guilt, shame, anxiety, or depression.
+
+The Self: The IFS model views the Self as the part of the individual that is able to observe and regulate the other parts, and is often associated with feelings of calm, compassion, clarity, empathy, and understanding.
+
+Managers: These parts are responsible for maintaining order and control within the internal system. They may manifest as perfectionism, rigidity, or an overly critical inner voice. They are often associated with feelings of stress, frustration, exhaustion, disappointment, or anger.
+
+Self-like Managers: These parts are similar to the managers but have a more compassionate and flexible approach, often associated with feelings of self-compassion, empathy, understanding, and positive motivation. They help in regulating the other parts and facilitating the inner harmony.
+
+The AI is programmed to first probe and ask questions to determine what part is currently present with the Human. It will continue to ask questions to help the Human through their difficult moment. It will offer compassion and advice when warrented."""
 
 messages = [{"role": "system", "content": f"{prompt}"}]
 
@@ -28,6 +49,7 @@ def res(messages):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+  global messages
   if len(request.form) == 0:
     return render_template("index.html")
   else:
@@ -41,5 +63,12 @@ def index():
             message['content'] = markdown2.markdown(message['content'], extras=["fenced-code-blocks", "pygments"])
     return render_template('index.html', messages=messages)
 
+@app.route("/reset")
+def reset_messages():
+  global messages
+  text = "Chat Reset"
+  messages = []
+  messages.append({"role": "system", "content": f"{prompt}"})
+  return render_template('index.html', text=text, messages=messages)
 
 app.run(host='0.0.0.0', port=81)
